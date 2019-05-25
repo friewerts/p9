160921162e90de3c8209bb082faa646b029d4b71
Frederik Riewerts <frederik.riewerts@gmail.com>
2019-05-25 19:14:22 +0200

    added testing package

---
+++ packages/test/src/test.js
@@ -0,0 +1,12 @@
+const {getValue} = require('@p9/js-utils/lib/object-helpers'); 
+
+const obj = {
+  a: {
+    b: {
+      c: 1
+    },
+    d: true
+  }
+};
+console.log('1', getValue(obj, "a.b"));
+console.log('2', getValue(obj, "a.f"));
